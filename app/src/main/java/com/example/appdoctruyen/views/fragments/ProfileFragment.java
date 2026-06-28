59c10e1d0cc59f47736bdbc2c72291f7832938fb
Pham Thanh An-23130002 <23130002@st.hcmuaf.edu.vn>
2026-06-28 23:39:53 +0700

    Xử lý lưu thông tin người dùng(tạm thời)

--- app/src/main/java/com/example/appdoctruyen/views/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/appdoctruyen/views/fragments/ProfileFragment.java
@@ -3,10 +3,12 @@ package com.example.appdoctruyen.views.fragments;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,11 +21,15 @@ import com.example.appdoctruyen.views.activities.RechargeActivity;
 import com.example.appdoctruyen.views.activities.SearchActivity;
 import com.example.appdoctruyen.views.activities.UserDetailsActivity;
 import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ProfileFragment extends Fragment {
     LinearLayout btn_logout, btn_topup;
     ShapeableImageView iv_user_avatar;
     private AuthManager authManager;
+    private TextView tv_username;
 
     @SuppressLint("MissingInflatedId")
     @Nullable
@@ -34,6 +40,8 @@ public class ProfileFragment extends Fragment {
         btn_logout = view.findViewById(R.id.btn_logout);
         btn_topup = view.findViewById(R.id.btn_topup);
         iv_user_avatar = view.findViewById(R.id.iv_user_avatar);
+        tv_username = view.findViewById(R.id.tv_username);
+        loadUserProfile();
         authManager = new AuthManager();
 
         btn_logout.setOnClickListener(v -> {
@@ -53,5 +61,31 @@ public class ProfileFragment extends Fragment {
             startActivity(intent);
         });
         return view;
+    }
+    private void loadUserProfile() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String uid = currentUser.getUid();
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            db.collection("users").document(uid)
+                    .addSnapshotListener((documentSnapshot, error) -> {
+                        if (error != null) {
+                            Log.e("ProfileFragment", "Lỗi tải dữ liệu", error);
+                            return;
+                        }
+                        if (documentSnapshot != null && documentSnapshot.exists()) {
+                            String name = documentSnapshot.getString("username");
+
+                            if (name != null && !name.isEmpty()) {
+                                tv_username.setText(name);
+                            } else {
+                                String email = currentUser.getEmail();
+                                if (email != null && email.contains("@")) {
+                                    tv_username.setText(email.substring(0, email.indexOf("@")));
+                                }
+                            }
+                        }
+                    });
+        }
     }
 }
